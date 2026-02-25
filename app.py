7d403004934bcc2d6db026a432fe59b87ea5cec1
Sravani-excelr <panam.sravani@excelr.com>
2026-02-25 13:44:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+#"C:\Users\excel\OneDrive\Documents\Flask-SMS_classifier\app.py"
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import joblib
+
+
+app = Flask(__name__)
+
+#decorator (@app.route('/')) to specify the URL that should trigger the execution of the home function
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	df= pd.read_csv(r"C:\Users\excel\OneDrive\Documents\Flask-SMS_classifier\spam.csv", encoding="latin-1")
+
+	
+	# Features and Labels
+    
+    
+	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+	X = df['message']
+	y = df['label']
+	
+	# Extract Feature With CountVectorizer
+	cv = CountVectorizer()
+	X = cv.fit_transform(X) # Fit the Data
+
+	from sklearn.model_selection import train_test_split
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+	#Naive Bayes Classifier
+	from sklearn.naive_bayes import MultinomialNB
+
+	clf = MultinomialNB()
+	clf.fit(X_train,y_train)
+	clf.score(X_test,y_test)
+	#Alternative Usage of Saved Model
+	# joblib.dump(clf, 'NB_spam_model.pkl')
+	# NB_spam_model = open('NB_spam_model.pkl','rb')
+	# clf = joblib.load(NB_spam_model)
+
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
